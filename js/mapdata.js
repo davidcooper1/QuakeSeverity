aeb120f30d15448f4c23dd94394db8f1b1d7760c
davidcooper1 <davidpcooper1@gmail.com>
2019-05-10 01:01:09 -0500

    Added comments to mapdata.js.

--- js/mapdata.js
+++ js/mapdata.js
@@ -1,16 +1,22 @@
+// Class made to create SVGs to display St. Himark maps.
 class MapData {
 
   constructor(width, height) {
+    // Viewbox width and height.
     this.width = width;
     this.height = height;
 
     this.svg = document.createElementNS(d3.namespaces.svg, "svg");
+
+    // Sets viewBox dimensions for SVG instead of width and height. This allows
+    // SVGs to be resized with css dynamically.
     this.content = d3.select(this.svg)
       .attr("viewBox", "0 0 " + this.width + " " + this.height)
       .append("g");
 
     this.colorScale = d3.scaleSequential(d3.interpolateYlOrRd).domain([0,10]);
 
+    // Creates the geoMercator projection object to convert map coordinates.
     this.projection = d3.geoMercator()
       .center([-119.847,0.11804])
       .scale(80000)
@@ -21,6 +27,7 @@ class MapData {
     this.data = []
   }
 
+  // Function to initialize/draw the map.
   draw() {
     if (!this.paths) {
       this.paths = this.content
@@ -31,7 +38,7 @@ class MapData {
         .attr("d", this.pathBuilder)
         .style("stroke", "black")
         .style("fill", "white")
-        .style("pointer-events", "all")
+        .style("pointer-events", "all") // Needed to capture ANY mouse events.
         .attr("data-toggle", "tooltip")
         .attr("title", function(d) { return d.properties.Nbrhood; })
         .on("mouseover", function(d) {
@@ -40,6 +47,7 @@ class MapData {
     }
   }
 
+  // Add event listener to every path in the map.
   on(type, func) {
     console.log("called");
     if (typeof type === "string") {
@@ -54,6 +62,8 @@ class MapData {
     }
   }
 
+  // Set the color of a specific path based on the intensity provided.
+  // Intensities should be within the range 0 to 10.
   setIntensity(id, intensity) {
     let self = this;
     this.paths.each(function(d) {
@@ -63,6 +73,7 @@ class MapData {
     });
   }
 
+  // Set the color of a specific path.
   setColor(id, color) {
     let self = this;
     console.log(color)
@@ -73,6 +84,8 @@ class MapData {
     });
   }
 
+
+  // Gets the ids of all paths in the map.
   getPathIds() {
     let ids = [];
     this.paths.each(function(d) {
