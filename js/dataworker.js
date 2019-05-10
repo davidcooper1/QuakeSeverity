343e54969147202c980ec7b4e6af66ea0ba494ba
davidcooper1 <davidpcooper1@gmail.com>
2019-05-10 00:53:39 -0500

    Added comments to dataworker.js.

--- js/dataworker.js
+++ js/dataworker.js
@@ -3,12 +3,16 @@ importScripts("./messagehelper.js");
 
 let db;
 
+// Handle messages coming from webpage.
 self.addEventListener("message", function(event) {
   let message = event.data;
   if (message[0] === "init") {
     loadDBObject();
   } else if (message[0] == "getdaterange") {
+    // Get the range of times for all the reports.
     let result = db.exec("SELECT MIN(time_of_report) AS earliest, MAX(time_of_report) AS latest FROM grouped_reports");
+
+    // Return the range to the webpage.
     postMessage(new WorkerMessage(
       WorkerMessage.TYPE_DATA,
       "range",
@@ -19,6 +23,9 @@ self.addEventListener("message", function(event) {
     let category = message[1];
     let start = message[2];
     let end = message[3];
+
+    // Get average intensity of reports in the specified time frame for the
+    // specified neighborhood and category.
     let result = db.exec(`
       SELECT id, COALESCE(category, 0) FROM neighborhoods
         LEFT JOIN (SELECT neighborhood_id, AVG(${category}) AS category FROM grouped_reports
@@ -28,8 +35,9 @@ self.addEventListener("message", function(event) {
 
     let values = result[0].values;
 
+    // Return the average intensities of each neighborhood to the webpage.
     for (let i = 0; i < values.length; i++) {
-      // intensity header, category, neighborhood id, intensity
+      // Message data: intensity header, category, neighborhood id, intensity
       postMessage(new WorkerMessage(
         WorkerMessage.TYPE_DATA,
         "intensity",
@@ -42,6 +50,10 @@ self.addEventListener("message", function(event) {
     let category = message[1];
     let neighborhood_id = message[2];
 
+    // Get the set of intensities from avg_reports table for a neighborhood and category.
+
+    // avg_reports intensities are averaged with all reports within 30 minutes before or after
+    // the current report.
     let result = db.exec(`
       SELECT time_of_report, ${category} FROM avg_reports
         WHERE neighborhood_id = ${neighborhood_id}
@@ -50,6 +62,7 @@ self.addEventListener("message", function(event) {
 
     let values = result[0].values;
 
+    // Return the set of intensities to the webpage.
     postMessage(new WorkerMessage(
       WorkerMessage.TYPE_DATA,
       "time_data",
@@ -61,11 +74,14 @@ self.addEventListener("message", function(event) {
 });
 
 function loadDBObject() {
+  // Load the sql-wasm module.
   initSqlJs({locateFile: fileName => `../bin/${fileName}`}).then(function(SQL) {
+    // Load the sqlite database file.
     let xhr = new XMLHttpRequest();
     xhr.open("GET", "../data/report-data.sqlite", true);
     xhr.responseType = "arraybuffer";
     xhr.onload = e => {
+      // Store database in the global scope.
       db = new SQL.Database(new Uint8Array(e.target.response));
       postMessage(new WorkerMessage(WorkerMessage.TYPE_STATUS, WorkerMessage.STATUS_READY));
     }
