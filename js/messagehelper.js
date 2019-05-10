bff3b1d18c0084e8cb302f6b5d943250249206b4
davidcooper1 <davidpcooper1@gmail.com>
2019-05-10 01:03:00 -0500

    Added comments to messagehelper.js.

--- js/messagehelper.js
+++ js/messagehelper.js
@@ -1,3 +1,4 @@
+// Class made to wrap data sent back to webpage.
 class WorkerMessage {
 
   constructor(type, ...args) {
@@ -7,10 +8,12 @@ class WorkerMessage {
 
 }
 
+// Status IDs for messages of TYPE_STATUS.
 WorkerMessage.STATUS_READY = 0;
 WorkerMessage.STATUS_LOADING = 1;
 WorkerMessage.STATUS_UPDATING = 2;
 
+// Message headers to denote message types.
 WorkerMessage.TYPE_STATUS         = "status";
 WorkerMessage.TYPE_ERROR          = "error";
 WorkerMessage.TYPE_PROGRESS_FIN   = "pfin";
